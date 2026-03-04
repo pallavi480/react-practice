8299a9f4c2a0a0344951eef6b6786db28dfc6745
Pallavi480 <pallavigayaki@gmail.com>
2026-03-04 14:40:56 +0530

    updated todo app

--- Todo app/src/components/TodoItem.jsx
+++ Todo app/src/components/TodoItem.jsx
@@ -1,34 +1,35 @@
-import React,{ useState} from "react";
+import React, { useState } from "react";
 
-function TodoItem({ todo, delatedTodo, editTodo }) {
-    const [isEditing, setIsEditing] = useState(false);
-    const [newText, setNewText] = useState(todo.text);
+function TodoItem({ todo, deleteTodo, editTodo }) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [newText, setNewText] = useState(todo.text);
 
-    const handleEdit = () => {
-        editTodo(todo.id, newText);
-        setIsEditing(false);
-    }
+  const handleEdit = () => {
+    if (!newText.trim()) return;
+    editTodo(todo.id, newText);
+    setIsEditing(false);
+  };
 
-    return (
-        <div className="todo-item">
-            {isEditing ? (
-                <>
-                    <input
-                       value={newText}
-                       onChange={(e) => setNewText(e.target.value)}
-                    />
-                    <button onClick={handleEdit}>Save</button>
-                </>
-            ) : (
-                <>
-
-                    <span>{todo.text}</span>
-                    <button onClick={() => setIsEditing(true)}>Edit</button>
-                    <button onClick={() => delatedTodo(todo.id)}>Delete</button>
-                </>
-            )}
-        </div>
-    )
+  return (
+    <div style={{ marginBottom: "10px" }}>
+      {isEditing ? (
+        <>
+          <input
+            type="text"
+            value={newText}
+            onChange={(e) => setNewText(e.target.value)}
+          />
+          <button onClick={handleEdit}>Save</button>
+        </>
+      ) : (
+        <>
+          <span>{todo.text}</span>
+          <button onClick={() => setIsEditing(true)}>Edit</button>
+          <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+        </>
+      )}
+    </div>
+  );
 }
 
 export default TodoItem;
--- Todo app/src/components/TodoList.jsx
+++ Todo app/src/components/TodoList.jsx
@@ -1,19 +1,19 @@
 import React from "react";
 import TodoItem from "./TodoItem";
 
-function TodoList({ todos, delatedTodo, editTodo }) {
-    return (
-        <div>
-            {todos.map((todo) => (
-                <TodoItem
-                    key={todo.id}
-                    todo={todo}
-                    delatedTodo={delatedTodo}
-                    editTodo={editTodo}
-                />
-            ))}
-        </div>
-    )
+function TodoList({ todos, deleteTodo, editTodo }) {
+  return (
+    <div>
+      {todos.map((todo) => (
+        <TodoItem
+          key={todo.id}
+          todo={todo}
+          deleteTodo={deleteTodo}
+          editTodo={editTodo}
+        />
+      ))}
+    </div>
+  );
 }
 
 export default TodoList;
