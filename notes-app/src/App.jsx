009cfd037ce9d8f99b11598494ac4f0a410f1299
Pallavi480 <pallavigayaki@gmail.com>
2026-03-07 12:52:11 +0530

    updated app.jsx

--- notes-app/src/App.jsx
+++ notes-app/src/App.jsx
@@ -1,34 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import NoteForm from "./components/NoteForm";
+import NoteList from "./components/NoteList";
+import { NotesContext } from "./context/NotesContext";
+import useLocalStorage from "./hooks/useLocalStorage";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [notes, setNotes] = useLocalStorage("notes",[])
 
+  const addNote = (text) =>{
+    setNotes([...notes, {id: Date.now(), text}])
+  }
+
+  const deleteNote = (id) => {
+    setNotes(notes.filter((note)=> note.id !== id))
+  }
   return (
-    <>
+    <NotesContext.Provider value = {{notes, addNote, deleteNote}}>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>Notes App</h1>
+        <NoteForm />
+        <NoteList />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </NotesContext.Provider>
+
   )
 }
 
